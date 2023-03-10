a13e788d552b7c21ad4aa6028df926349d88978c
a-maczynska <a.maczynskaa@gmail.com>
2023-03-10 19:12:00 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,14 @@
-let currencyPlnElement = document.querySelector(".js-currencyPln");
-let formElement = document.querySelector(".js-form");
-let eurElement = document.querySelector(".js-result");
+function change() {
+  const currencyPlnElement = document.querySelector(".js-currencyPln");
+  const eurElement = document.querySelector(".js-result");
+  const pln = currencyPlnElement.value;
+  const currencyEurElement = 0.21;
+  const eur = pln * currencyEurElement;
 
+  return (eurElement.innerText = `${pln} PLN to ${eur.toFixed(2)} EUR`);
+}
+const formElement = document.querySelector(".js-form");
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  let pln = currencyPlnElement.value;
-  let currencyEurElement = 0.21;
-  let eur = pln * currencyEurElement;
-
-  eurElement.innerText = `${pln} PLN to ${eur.toFixed(2)} EUR`;
+  change();
 });
