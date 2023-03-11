dd7f365271e66b395160b799aea319be4623ac12
a-maczynska <a.maczynskaa@gmail.com>
2023-03-11 10:32:07 +0100

    added 3 different currencies

--- src/script.js
+++ src/script.js
@@ -1,16 +1,38 @@
-{
-  function change() {
-    const currencyPlnElement = document.querySelector(".js-currencyPln");
-    const eurElement = document.querySelector(".js-result");
-    const pln = currencyPlnElement.value;
-    const currencyEurElement = 0.21;
-    const eur = pln * currencyEurElement;
+const resultText = (amount, currency, result) => {
+  const resultElement = document.querySelector(".js-result");
+  resultElement.innerHTML = `${amount.toFixed(
+    2
+  )} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>
+`;
+};
 
-    return (eurElement.innerText = `${pln} PLN to ${eur.toFixed(2)} EUR`);
+const convert = (amount, currency) => {
+  const EUR = 4.68;
+  const GBP = 5.29;
+  const USD = 4.4;
+  switch (currency) {
+    case "EUR":
+      return amount / EUR;
+    case "GBP":
+      return amount / GBP;
+    case "USD":
+      return amount / USD;
   }
+};
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  const amountElement = document.querySelector(".js-currencyPln");
+  const currencyElement = document.querySelector(".js-form__select");
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
+  let result = convert(amount, currency);
+  resultText(amount, currency, result);
+};
+
+const handleSubmit = () => {
   const formElement = document.querySelector(".js-form");
-  formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    change();
-  });
-}
+  formElement.addEventListener("submit", onFormSubmit);
+};
+
+handleSubmit();
